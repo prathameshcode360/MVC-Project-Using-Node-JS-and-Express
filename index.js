b56d4dcfc60741a725dd8285c6cac399a1e49a50
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-03-05 20:33:42 +0530

    added ejs view engine for dynamic view

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import Controller from "./src/controllers/product.controller.js";
+import path from "path";
 
 const server = express();
 
 const controller = new Controller();
-
+// setting up static view
 server.use(express.static("src/views"));
 
-server.get("/view", controller.getView);
-server.get("/products", controller.getProducts);
+// server.get("/view", controller.getView);
 
-server.get("/", (req, res) => {
-  res.send("Welcome to node JS server");
-});
+// setting ejs view engine
+server.set("view engine", "ejs");
+server.set("views", path.join(path.resolve(), "src", "views"));
+
+server.get("/", controller.getProducts);
+
+// server.get("/", (req, res) => {
+//   res.send("Welcome to node JS server");
+// });
 
 server.listen(3000, () => {
   console.log("Server is listening on port 3000");
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,15 +1,13 @@
-import path from "path";
 import ProductModel from "../models/product.model.js";
 
 export default class Controller {
-  getView(req, res) {
-    return res.sendFile(
-      path.join(path.resolve(), "src", "views", "index.html")
-    );
-  }
+  // getView(req, res) {
+  //   return res.sendFile(
+  //     path.join(path.resolve(), "src", "views", "index.html")
+  //   );
+  // }
   getProducts(req, res) {
     const products = ProductModel.getProducts();
-    console.log(products);
-    return res.status(200).send({ products: products });
+    return res.render("index", { products: products });
   }
 }
